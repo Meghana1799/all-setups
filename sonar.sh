6e8afb56d8fdca0278545e9c368307b9ff2c2c94
Meghana <52189504+Meghana1799@users.noreply.github.com>
2026-02-25 11:00:06 +0530

    Update sonar.sh

--- sonar.sh
+++ sonar.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 #Launch an instance with 9000 and t2.medium
 cd /opt/
-wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-8.9.6.50800.zip
+wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.1.0.47736.zip
 unzip sonarqube-8.9.6.50800.zip
 yum install java-17-amazon-corretto -y
 useradd sonar
